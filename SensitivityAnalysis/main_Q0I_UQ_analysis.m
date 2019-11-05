455d36f30dc1ee3d800d45a9d59e45b19b72b30c
Unknown <harleyhanes97@gmail.com>
2019-11-05 11:08:52 -0600

    Made main function more concise and readable
    There is some redundancy between ContrainedTrans and InvestigateLambda SA tests

--- SensitivityAnalysis/main_Q0I_UQ_analysis.m
+++ SensitivityAnalysis/main_Q0I_UQ_analysis.m
@@ -3,12 +3,13 @@ function [POI,QOI]=main_Q0I_UQ_analysis(varargin)
 % interest (QOIs) as a function of input parameters of interest (POIs)
 
 if nargin>=1
-        str.QOI_model_name=varargin{1};
+        str.QOI_model_name=varargin{1};      %Model to run is first input
     if nargin>=2
-        str.ParamSettings=varargin{2};
+        str.ParamSettings=varargin{2};       %parameter settings are second input
     end
 else
-    str.QOI_model_name='Chagas-Gen1-AssessLambda';
+    str.QOI_model_name='Chagas-Gen1-Full';   %Model to run if no inputs
+    str.ParamSettings='base';
 end
 
 % the user must a code to generate the QOIs from the POIs 
@@ -38,13 +39,13 @@ str= QOI_change_default_params(str);% user code to change the default parameter 
 %  str.QOI_pre_analysis(str);
 % 
 % % 2. local sensitivity analysis
-% [USI,RSI]=str.QOI_LSA(str); POI.LSA=POI_LSA; QOI.LSA=QOI_LSA;
+ [POI_LSA,QOI_LSA]=str.QOI_LSA(str); POI.LSA=POI_LSA; QOI.LSA=QOI_LSA;
 % 
 % % 3. extended sensitivity analysis
  [POI_ESA,QOI_ESA]=str.QOI_ESA(str); POI.ESA=POI_ESA; QOI.ESA=QOI_ESA;
 % 
 % %4. global sensitivity analysis
-% [POI_GSA,QOI_GSA]=str.QOI_GSA(str); POI.GSA=POI_GSA; QOI.GSA=QOI_GSA;
+ [POI_GSA,QOI_GSA]=str.QOI_GSA(str); POI.GSA=POI_GSA; QOI.GSA=QOI_GSA;
 
 %global sensitivity sobol indices
 % [sobol_indices]=Sobol_GSA(str);
@@ -57,36 +58,41 @@ end
 
 function str= QOI_change_default_params(str)
 %% QOI_change_default_params change default parameters
+%Load parameters
+    params=baseline_params(str.ParamSettings);
+        theta=params.theta;
+        lambda=params.lambda;
+        mu=params.mu;
 switch str.QOI_model_name
     case 'Chagas-Gen1-lambda'
+        %Select POI's and QOI's
         str.POI_names =  {'\lambda_H','\lambda_V'};
                       str.nPOI=length(str.POI_names);
-        POInames = str.POI_names;
         str.QOI_names =  {'Proportion I_{DV} at equilibirium','Proportion I_{DV} at t=5', 'R_0'};
         str.nQOI=length(str.QOI_names);
-        QOInames=str.QOI_names;
+        
+        %SelectModel 
         str.QOI_model_eval = @BBB_Chagas_Gen1_model;
-        str.POI_baseline=[.05 .05]';
+        
+        %Set parameter Sampling
+        str.POI_baseline=[lambda.H lambda.V]';
         if length(str.POI_baseline)~=str.nPOI
             fprintf('ERROR!!! Different number of parameters named than entered\n')
         end
         str.POI_min=str.POI_baseline-.8*str.POI_baseline;
         str.POI_max=str.POI_baseline+.8*str.POI_baseline;
         str.POI_mode=str.POI_baseline;
-        str.POI_pdf='beta';% uniform triangle beta
-        str.number_ESA_samples = 30;
     case 'Chagas-Gen1-PopParm'
+        %Select POIs and QOIs
         str.POI_names =  {'\lambda_H','\lambda_V','\mu_{SH}','\mu_{SV}','\mu_{DH}','\mu_{DV}'};
-                      str.nPOI=length(str.POI_names);
-        POInames = str.POI_names;
+              str.nPOI=length(str.POI_names);
         str.QOI_names = {'Proportion I_{DV} at equilibirium','Proportion I_{DV} at t=5', 'R_0'};
-        str.nQOI=length(str.QOI_names);
-        QOInames=str.QOI_names;
+            str.nQOI=length(str.QOI_names);
+        
+        %Select Model
         str.QOI_model_eval = @BBB_Chagas_Gen1_model;        
-        params=baseline_params();
 
-        lambda=params.lambda;
-        mu=params.mu;
+        %Set Parameter Ranges
         str.POI_baseline=[lambda.H lambda.V mu.SH mu.SV mu.DH mu.DV ]';
         %str.POI_baseline=[.05 .05 .83/365 .271/365 .83/365 .271/365 ]';
         if length(str.POI_baseline)~=str.nPOI
@@ -94,23 +100,22 @@ switch str.QOI_model_name
         end
         str.POI_min=str.POI_baseline-.8*str.POI_baseline;
         str.POI_max=str.POI_baseline+.8*str.POI_baseline;
-        str.POI_min(1:2)=0;
-        str.POI_max(1:2)=2*str.POI_baseline(1:2);
+        str.POI_min(1:2)=0;                      %Set a wider sampling range
+        str.POI_max(1:2)=2*str.POI_baseline(1:2);%for lambdas
         str.POI_mode=str.POI_baseline;
+        %Set Sampling
         str.POI_pdf='beta';% uniform triangle beta
         str.number_ESA_samples = 30;
     case 'Chagas-Gen1-Thetas'
+        %Select POIs and QOIs
         str.POI_names =  {'\theta^{SV}_{SH}','\theta^{DV}_{SH}','\theta^{SH}_{SV}','\theta^{DH}_{SV}',...
                           '\theta^{SV}_{DH}','\theta^{DV}_{DH}','\theta^{SH}_{DV}','\theta^{DH}_{DV}'};
-            %NAMES MUST MATCH THOSE IN get_p_struct
-        str.nPOI=length(str.POI_names);
-        POInames = str.POI_names;
+            str.nPOI=length(str.POI_names);
         str.QOI_names =  {'Proportion I_{DV} at equilibirium','Proportion I_{DV} at t=5', 'R_0'};
-        str.nQOI=length(str.QOI_names);
-        QOInames=str.QOI_names;
+            str.nQOI=length(str.QOI_names);
         str.QOI_model_eval = @BBB_Chagas_Gen1_model;
-        params=baseline_params();
-        theta=params.theta;
+        
+        %Set parameter ranges
         str.POI_baseline=[theta.SV_SH theta.DV_SH theta.SH_SV theta.DH_SV...
                           theta.SV_DH theta.DV_DH theta.SH_DV theta.DH_DV]';
         %str.POI_baseline=[.01 .002 .01 .002 .002 .01 .002 .01]';
@@ -120,23 +125,19 @@ switch str.QOI_model_name
         str.POI_min=str.POI_baseline-.5*str.POI_baseline;
         str.POI_max=str.POI_baseline+.5*str.POI_baseline;
         str.POI_mode=str.POI_baseline;
-        str.POI_pdf='beta';% uniform triangle beta
-        str.number_ESA_samples = 30;
     case 'Chagas-Gen1-Full'
+        %Select POIs and QOIs
         str.POI_names =  {'\theta^{SV}_{SH}','\theta^{DV}_{SH}','\theta^{SH}_{SV}','\theta^{DH}_{SV}',...
                           '\theta^{SV}_{DH}','\theta^{DV}_{DH}','\theta^{SH}_{DV}','\theta^{DH}_{DV}',...
                           '\lambda_H','\lambda_V','\mu_{SH}','\mu_{SV}','\mu_{DH}','\mu_{DV}'};
-            %NAMES MUST MATCH THOSE IN get_p_struct
-        str.nPOI=length(str.POI_names);
-        POInames = str.POI_names;
+            str.nPOI=length(str.POI_names);
         str.QOI_names =  {'Proportion I_{DV} at equilibirium','Proportion I_{DV} at t=5', 'R_0'};
-        str.nQOI=length(str.QOI_names);
-        QOInames=str.QOI_names;
+            str.nQOI=length(str.QOI_names);
+        
+        %Select Model
         str.QOI_model_eval = @BBB_Chagas_Gen1_model;
-        params=baseline_params();
-        theta=params.theta;
-        lambda=params.lambda;
-        mu=params.mu;
+        
+        %Set ESA and GSA Ranges
         str.POI_baseline=[theta.SV_SH theta.DV_SH theta.SH_SV theta.DH_SV...
                           theta.SV_DH theta.DV_DH theta.SH_DV theta.DH_DV ...
                           lambda.H lambda.V mu.SH mu.SV mu.DH mu.DV ]';
@@ -149,24 +150,20 @@ switch str.QOI_model_name
         str.POI_min(9:10)=0;
         str.POI_max(9:10)=2*str.POI_baseline(9:10);
         str.POI_mode=str.POI_baseline;
-        str.POI_pdf='beta';% uniform triangle beta
-        str.number_ESA_samples = 30;
-    case 'Chagas-Gen1-ConstrainedTrans'
+    case 'Chagas-Gen1-ConstrainedTrans' %Note: meant to be used with InvestigateLambda
+        %Select POIs and QOIs
         str.POI_names =  {'\theta^{SV}_{SH}','\theta^{SH}_{SV}',...
                           '\theta^{DV}_{DH}','\theta^{DH}_{DV}',...
                           '\lambda_H','\lambda_V','\mu_{SH}',...
                           '\mu_{SV}','\mu_{DH}','\mu_{DV}'};
-            %NAMES MUST MATCH THOSE IN get_p_struct
-        str.nPOI=length(str.POI_names);
-        POInames = str.POI_names;
+            str.nPOI=length(str.POI_names);
         str.QOI_names =  {'Proportion I_{DV} at equilibirium','Proportion I_{DV} at t=5', 'R_0'};
-        str.nQOI=length(str.QOI_names);
-        QOInames=str.QOI_names;
+            str.nQOI=length(str.QOI_names);
+            
+        %Select Model
         str.QOI_model_eval = @BBB_Chagas_Gen1_model;
-        params=baseline_params();
-        theta=params.theta;
-        lambda=params.lambda;
-        mu=params.mu;
+        
+        %Set Parameter Settings
         str.POI_baseline=[theta.SV_SH  theta.SH_SV theta.DV_DH  theta.DH_DV ...
                           lambda.H lambda.V mu.SH mu.SV mu.DH mu.DV ]';
         %str.POI_baseline=[.01 .002 .01 .002 .002 .01 .002 .01]';
@@ -178,20 +175,17 @@ switch str.QOI_model_name
         str.POI_min(5:6)=0;
         str.POI_max(5:6)=str.POI_baseline(5:6);
         str.POI_mode=str.POI_baseline;
-        str.POI_pdf='beta';% uniform triangle beta
-        str.number_ESA_samples = 40;
     case 'Chagas-Gen1-AssessLambda'
+        %Select POIs and QOIs
         str.POI_names =  {'\lambda_H','\lambda_V'};
-            %NAMES MUST MATCH THOSE IN get_p_struct
-        str.nPOI=length(str.POI_names);
-        str.select.POI=str.POI_names;
+            str.nPOI=length(str.POI_names);
         str.QOI_names =  {'Proportion I_{DV} at equilibirium','Proportion I_{DV} at t=5', 'R_0'};
-        str.nQOI=length(str.QOI_names);
-        str.select.QOI=str.QOI_names;
+            str.nQOI=length(str.QOI_names);
+        
+        %Select Model
         str.QOI_model_eval = @BBB_Chagas_Gen1_model;
-        ParamSettings=str.ParamSettings;
-        params=baseline_params(ParamSettings);
-        lambda=params.lambda;
+        
+        %Set Parameter Ranges
         str.POI_baseline=[lambda.H lambda.V]';
         %str.POI_baseline=[.01 .002 .01 .002 .002 .01 .002 .01]';
         if length(str.POI_baseline)~=str.nPOI
@@ -200,12 +194,18 @@ switch str.QOI_model_name
         str.POI_min=zeros(2,1);
         str.POI_max=2*str.POI_baseline;
         str.POI_mode=str.POI_baseline;
-        str.POI_pdf='beta';% uniform triangle beta
-        str.number_ESA_samples = 15;
     otherwise
         error([' str.QOI_model =',str.QOI_model,' is not available'])
 end
 
+%Set Common Properties
+    %Sampling
+    str.POI_pdf='beta';% uniform triangle beta
+    str.number_ESA_samples = 15;
+    
+    %Pass selected parameters to ODE
+    str.select.POI=str.POI_names;
+    str.select.QOI=str.QOI_names;
 end
 
 
